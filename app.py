27286967b5a4a07b00002b862cef1d63cac9f694
zeynelg <89345444+zeynelg@users.noreply.github.com>
2023-06-03 06:16:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True,host='0.0.0.0',port=port)
 my_logger = logging.getLogger('test_logger')
-my_logger.setLevel(logging.DEBUG)
-
-handler = graypy.GELFTCPHandler('192.168.49.1', 12201)
-my_logger.addHandler(handler)
-
-my_logger.debug('This is a test from a Python script!')
+handler = graypy.GELFHandler('192.168.49.1', 12201)
+while True:
+    my_logger.debug('Danger, Will Robinson, Danger!') 
+    time.sleep(1)
