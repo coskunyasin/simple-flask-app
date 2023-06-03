9754fce524857e0b747712974663eb9924b176c7
zeynelg <89345444+zeynelg@users.noreply.github.com>
2023-06-03 06:36:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 import logging
-import graypy
 import os
 
 app = Flask(__name__)
@@ -13,8 +12,3 @@ def hello():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True,host='0.0.0.0',port=port)
-my_logger = logging.getLogger('test_logger')
-handler = graypy.GELFTCPHandler('192.168.49.1', 12201)
-while True:
-    my_logger.debug('Danger, Will Robinson, Danger!') 
-    time.sleep(1)
