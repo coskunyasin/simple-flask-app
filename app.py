d12040daaf73896edb439dc4f5795688fbf9542a
zeynelg <89345444+zeynelg@users.noreply.github.com>
2023-06-03 05:44:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from pygelf import GelfTcpHandler
+import logging
 import os
 
 app = Flask(__name__)
@@ -11,3 +13,7 @@ def hello():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True,host='0.0.0.0',port=port)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+logger.addHandler(GelfTcpHandler(host='192.168.49.1', port=12201))
+logger.info('hello gelf')
